90af3eaca3dcf4a507ce0c9d991ca62b7431fb7d
Long Phạm Tiến <phamtienlong0309@gmail.com>
2023-06-28 21:24:41 +0700

    end task 7

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=shalltearbloodfallen01/prj4:v1.0.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run prj4 --image=$dockerpath --port=80 --labels project=prj4
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward prj4 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=shalltearbloodfallen01/prj4:v1.0.0
 
 # Step 2:  
-# Authenticate & tag
+# Authenticate
+docker login
+#tag
+docker tag prj4 $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
